d21a43efbcf1dcd8c8c18e05345bb302986125a5
Vihaan Solanki <vihaanssolanki@gmail.com>
2022-12-10 18:32:55 -0500

    Code changed to read a file

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+const fs = require('fs');
+const readline = require('readline');
+
+async function processLineByLine() {
+  const fileStream = fs.createReadStream('./countrycapitals.csv');
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+  // Note: we use the crlfDelay option to recognize all instances of CR LF
+  // ('\r\n') in input.txt as a single line break.
+
+  for await (const line of rl) {
+    // Each line in input.txt will be successively available here as `line`.
+    let stringParts = line.split(",");
+    console.log(stringParts[0], stringParts[1])
+    
+  }
+}
+
+processLineByLine();
